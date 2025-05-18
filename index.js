ed18e95b0629501a2035753ca5a9e20b57d703cb
minhvuong123 <minhvuong122222@gmail.com>
2025-05-18 16:58:38 +0700

    ui for play control

--- index.js
+++ index.js
@@ -19,6 +19,25 @@
         }
       }, 3000)
     })
+
+    let scrollTop = 0;
+    const zingContentElement = document.querySelector('.zing-content');
+    zingContentElement.addEventListener('scroll', (event) => {
+      const trackScroll = document.querySelector('.zing-content .scroll-track');
+      if (scrollTop < event.target.scrollTop) {
+        console.log('scrolling to bottom')
+        const trackHeight = event.target.clientHeight;
+        trackScroll.style.height = trackHeight + event.target.scrollTop + 'px';
+      } else {
+        console.log('scrolling to top')
+        if (event.target.scrollTop === 0) {
+          scrollTop = 0;
+          const trackHeight = event.target.clientHeight;
+          trackScroll.style.height = trackHeight + 'px';
+        }
+      }
+      scrollTop = event.target.scrollTop;
+    })
   }
 })();
 
@@ -43,6 +62,59 @@
         }
       }, 3000)
     })
+
+    let scrollTop = 0;
+    const zingContentElement = document.querySelector('.zing-playlist');
+    zingContentElement.addEventListener('scroll', (event) => {
+      const trackScroll = document.querySelector('.zing-playlist .scroll-track');
+      if (scrollTop < event.target.scrollTop) {
+        console.log('scrolling to bottom')
+        const trackHeight = event.target.clientHeight;
+        trackScroll.style.height = trackHeight + event.target.scrollTop + 'px';
+      } else {
+        console.log('scrolling to top')
+        if (event.target.scrollTop === 0) {
+          scrollTop = 0;
+          const trackHeight = event.target.clientHeight;
+          trackScroll.style.height = trackHeight + 'px';
+        }
+      }
+      scrollTop = event.target.scrollTop;
+    })
   }
 })();
+
+(function calculateValue(){
+  const progressThumb = document.querySelector('.progress-thumb');
+
+  progressThumb.addEventListener('click', (e) => {
+    if (e) {
+      e.stopPropagation();
+      const boundingClient = e.target.getBoundingClientRect();
+      const percentage = (e.clientX - boundingClient.left) / boundingClient.width * 100;
+      const progressBar = document.querySelector('.progress-bar');
+      if (progressBar) {
+        progressBar.style.setProperty('--progressValue', percentage + '%');
+      }
+    }
+  })
+})()
+
+
+function handlePlayList(){
+  const playlistControl = document.querySelector('.playlist-control');
+
+  playlistControl.addEventListener('click', () => {
+    const zingPlaylist = document.querySelector('.zing-playlist');
+    if (zingPlaylist) {
+      if (zingPlaylist.style.display === 'none') {
+        zingPlaylist.style.setProperty('display', 'inline-block');
+      } else {
+        zingPlaylist.style.setProperty('display', 'none');
+      }
+    }
+  })
+}
+
+handlePlayList();
 
